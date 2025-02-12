0c365d38e06a04e1bf397a1df8d2fee2d635635c
Kharanshu Wanare <83946522+kharanshuw@users.noreply.github.com>
2025-02-13 00:58:13 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = AIzaSyCCDlkueB9u6HDpz6crrVIodSrumfmCveY;
+  const apiKey = ;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
